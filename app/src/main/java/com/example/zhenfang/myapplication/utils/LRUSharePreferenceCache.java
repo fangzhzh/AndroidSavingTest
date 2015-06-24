16e5e458b776e9b6c2dedf6649413cc5e6b42b20
zhangzf <zhangzf@garena.com>
2015-06-24 19:09:55 +0800

    simultaneous read.

--- app/src/main/java/com/example/zhenfang/myapplication/utils/LRUSharePreferenceCache.java
+++ app/src/main/java/com/example/zhenfang/myapplication/utils/LRUSharePreferenceCache.java
@@ -9,9 +9,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -81,15 +79,16 @@ public class LRUSharePreferenceCache {
 
     private void writeCacheToFile() {
         JSONArray array = new JSONArray();
-        for (Map.Entry<String, String> entry : mMap.entrySet()) {
+        List<String> keys = new ArrayList<>();
+        keys.addAll(mMap.keySet());
+        for (String key : keys) {
             try {
                 array.put(new JSONObject()
-                        .put(entry.getKey(), entry.getValue())
+                                .put(key, mCache.get(key))
                 );
             } catch (JSONException e) {
-                e.printStackTrace();
+                BBAppLogger.e(e);
             }
-
         }
         mStorage.syncWrite("filename", array.toString().getBytes(), true);
     }
